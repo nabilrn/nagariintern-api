0cddb721a00fbdd13c2799a55d5df4c2dbcd8733
Naufal <2211522020_naufal@student.unand.ac.id>
2025-01-07 14:45:28 +0700

    update permintaan

--- models/permintaan.js
+++ models/permintaan.js
@@ -1,64 +1,82 @@
-    'use strict';
-    const { Model } = require('sequelize');
+'use strict';
+const { Model } = require('sequelize');
 
-    module.exports = (sequelize, DataTypes) => {
-    class PermintaanMagang extends Model {
-        static associate(models) {
-        // Relasi ke tabel User
-        PermintaanMagang.belongsTo(models.User, {
-            foreignKey: 'userId',
-            as: 'user', // Alias untuk relasi
-        });
-        }
+module.exports = (sequelize, DataTypes) => {
+  class PermintaanMagang extends Model {
+    static associate(models) {
+      // Relasi ke tabel User
+      PermintaanMagang.belongsTo(models.User, {
+        foreignKey: 'userId',
+        as: 'user', // Alias untuk relasi
+      });
     }
+  }
 
-    PermintaanMagang.init(
-        {
-        userId: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-            model: 'users', // Nama tabel User
-            key: 'id',
-            },
+  PermintaanMagang.init(
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users', // Nama tabel User
+          key: 'id',
         },
-        tipePemohon: {
-            type: DataTypes.ENUM('siswa', 'mahasiswa'),
-            allowNull: false,
-        },
-        institusi: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        jurusan: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
-        alamat: {
-            type: DataTypes.TEXT,
-            allowNull: true,
-        },
-        statusPermohonan: {
-            type: DataTypes.ENUM('menunggu', 'disetujui', 'ditolak'),
-            defaultValue: 'menunggu',
-            allowNull: false,
-        },
-        fileLamaran: {
-            type: DataTypes.STRING, 
-            allowNull: true,
-        },
-        tanggalPengajuan: {
-            type: DataTypes.DATE,
-            defaultValue: DataTypes.NOW,
-        },
-        },
-        {
-        sequelize,
-        modelName: 'PermintaanMagang',
-        tableName: 'permintaan_magang',
-        timestamps: true,
-        }
-    );
+      },
+      tipePemohon: {
+        type: DataTypes.ENUM('siswa', 'mahasiswa'),
+        allowNull: false,
+      },
+      institusi: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      jurusan: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      alamat: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      statusPermohonan: {
+        type: DataTypes.ENUM('menunggu', 'disetujui', 'ditolak'),
+        defaultValue: 'menunggu',
+        allowNull: false,
+      },
+      fileLamaran: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      tanggalPengajuan: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      statusPersetujuanPSDM: {
+        type: DataTypes.ENUM('menunggu', 'disetujui', 'ditolak'),
+        defaultValue: 'menunggu',
+        allowNull: false,
+      },
+      waktuPersetujuanPSDM: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      statusPersetujuanPimpinan: {
+        type: DataTypes.ENUM('menunggu', 'disetujui', 'ditolak'),
+        defaultValue: 'menunggu',
+        allowNull: false,
+      },
+      waktuPersetujuanPimpinan: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'PermintaanMagang',
+      tableName: 'permintaan_magang',
+      timestamps: true,
+    }
+  );
 
-    return PermintaanMagang;
-    };
+  return PermintaanMagang;
+};
