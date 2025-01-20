ea755f8fec77ad26ab27a095a92224747afd2073
Naufal <2211522020_naufal@student.unand.ac.id>
2025-01-20 15:10:57 +0700

    generate-letter

--- routes/admin.js
+++ routes/admin.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { verifyToken } = require('../middleware/AuthMiddleWare');
 
-const { getAllUnitKerja, editKuotaUnitKerja } = require('../controllers/SuperAdminController');
+const { getAllUnitKerja, editKuotaUnitKerja,generateLetter } = require('../controllers/SuperAdminController');
 
 
 router.get('/unit-kerja', verifyToken, getAllUnitKerja);
 router.put('/unit-kerja/:id', verifyToken, editKuotaUnitKerja);
+router.post('/generate-letter', generateLetter);
 module.exports = router;    
